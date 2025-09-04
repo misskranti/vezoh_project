ec1029be1ca69382f996e0f1be40cf4591b5ed3b
Web-Developer-Nishant <124562481+Web-Developer-Nishant@users.noreply.github.com>
2025-09-05 01:19:51 +0530

    errorMiddleware file created, throwError function created,  installed express-validator package, validation added in Particular Service api

--- routes/services.js
+++ routes/services.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const { auth } = require("../middleware/auth.js");
-const { params } = require("express-validator");
+const { param } = require("express-validator");
 const {
   servicesList,
   particularService,
   addServices,
 } = require("../controllers/serviceController.js");
+const { throwError } = require("../middleware/errorMiddleware.js");
 
 router.post("/services/add-services", addServices);
 
 router.get("/services", auth, servicesList);
 
-router.get("/services/:service", auth, particularService);
+router.get(
+  "/services/:service",
+  auth,
+  param("service")
+    .trim()
+    .notEmpty()
+    .withMessage("service is required")
+    .bail()
+    .not()
+    .isInt()
+    .withMessage("Invalid service selection"),
+  throwError,
+  particularService
+);
 
 module.exports = router;
