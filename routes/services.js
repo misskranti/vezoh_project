e968922d73652cbd1f944f77120b364fef6765a8
Web-Developer-Nishant <124562481+Web-Developer-Nishant@users.noreply.github.com>
2025-09-05 01:07:22 +0530

    services route updated.

--- routes/services.js
+++ routes/services.js
@@ -1,12 +1,17 @@
-const express = require("express")
-const router = express.Router()
-const {auth} = require("../middleware/auth.js")
-const { servicesList, particularService, addServices } = require("../controllers/serviceController.js")
+const express = require("express");
+const router = express.Router();
+const { auth } = require("../middleware/auth.js");
+const { params } = require("express-validator");
+const {
+  servicesList,
+  particularService,
+  addServices,
+} = require("../controllers/serviceController.js");
 
-router.post("/services/add-services", addServices)
+router.post("/services/add-services", addServices);
 
-router.get("/services", servicesList)
+router.get("/services", auth, servicesList);
 
-router.get("/services/:service", particularService)
+router.get("/services/:service", auth, particularService);
 
-module.exports = router
+module.exports = router;
